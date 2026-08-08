5a55ae386308d2c90b03590984fa874c25dd6bd9
wanan1234 <wangxiaosi@outlook.com>
2026-08-08 09:49:08 +0800

    首次提交 CeruMusic 项目

---
+++ src/main/utils/musicSdk/kg/index.js
@@ -0,0 +1,27 @@
+import leaderboard from './leaderboard'
+import songList from './songList'
+import musicSearch from './musicSearch'
+import pic from './pic'
+import lyric from './lyric'
+import hotSearch from './hotSearch'
+import comment from './comment'
+import tipSearch from './tipSearch'
+
+const kg = {
+  tipSearch,
+  leaderboard,
+  songList,
+  musicSearch,
+  hotSearch,
+  comment,
+  getLyric(songInfo) {
+    return lyric.getLyric(songInfo)
+  },
+  getPic(songInfo) {
+    return pic.getPic(songInfo)
+  },
+  getMusicDetailPageUrl(songInfo) {
+    return `https://www.kugou.com/song/#hash=${songInfo.hash}&album_id=${songInfo.albumId}`
+  }
+}
+export default kg
---
+++ src/main/utils/musicSdk/tx/index.js
@@ -0,0 +1,24 @@
+import leaderboard from './leaderboard'
+import lyric from './lyric'
+import songList from './songList'
+import musicSearch from './musicSearch'
+import hotSearch from './hotSearch'
+import comment from './comment'
+import tipSearch from './tipSearch'
+
+const tx = {
+  tipSearch,
+  leaderboard,
+  songList,
+  musicSearch,
+  hotSearch,
+  comment,
+  getLyric(songInfo) {
+    // let singer = songInfo.singer.indexOf('、') > -1 ? songInfo.singer.split('、')[0] : songInfo.singer
+    return lyric.getLyric(songInfo)
+  },
+  getMusicDetailPageUrl(songInfo) {
+    return `https://y.qq.com/n/yqq/song/${songInfo.songmid}.html`
+  }
+}
+export default tx
